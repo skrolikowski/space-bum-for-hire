0de65363d8b1ee25b66322b141cf0975d641dba6
Shane Krolikowski <skrolikowski@gmail.com>
2020-01-06 09:10:13 -0800

    work

--- src/gamestates/init.lua
+++ src/gamestates/init.lua
@@ -1,5 +1,7 @@
 Gamestates = {
+	-- cutscenes
 	cut01   = require 'src.gamestates.cutscenes.01',
+	-- gameplay
 	space01 = require 'src.gamestates.gameplay.space01',
 	mount01 = require 'src.gamestates.gameplay.mount01',
 }
